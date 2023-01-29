be900b6a89b592af920a2429401a34fcb3087cb0
DevinJaxues <devinmonsen@yahoo.com>
2023-01-28 23:26:33 -0700

    Fixed Flask app.py routing bug

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, request, redirect
 from flask_pymongo import PyMongo
 import scraping
 
@@ -8,17 +8,19 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
+# define route for the HTML page
 @app.route("/")
 def index():
-   mars = mongo.db.mars.find_one()
-   return render_template("index.html", mars=mars)
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
 
+# set up scraping route
 @app.route("/scrape")
 def scrape():
-   mars = mongo.db.mars
-   mars_data = scraping.scrape_all()
-   mars.update({}, mars_data, upsert=True)
-   return "Scraping Successful!"
+    mars = mongo.db.mars
+    mars_data = scraping.scrape()
+    mars.update({}, mars_data, upsert=True)
+    return redirect("/")
 
 if __name__ == "__main__":
    app.run(debug=True)
